e2b9649f2a43ab4b9752292ee700dbefe39dcf35
Damian Myerscough <Damian.Myerscough@gmail.com>
2018-07-26 00:14:11 -0700

    Initial MiruSSH Client

---
+++ client/client.go
@@ -0,0 +1,106 @@
+package client
+
+import (
+	"crypto/rsa"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"bytes"
+	"net/url"
+)
+
+type SingedAuthResponse struct {
+	Username    string `json:"username"`
+	Certificate string `json:"certificate"`
+	Ttl         uint32 `json:"ttl"`
+}
+
+type TokenAuthResponse struct {
+	Token string `json:"token"`
+}
+
+func ConfigureSSHAgent(key *rsa.PrivateKey, certificate *ssh.Certificate, username string, ttl uint32) {
+	addedKey := agent.AddedKey{
+		PrivateKey:   key,
+		Certificate:  certificate,
+		Comment:      username,
+		LifetimeSecs: ttl,
+	}
+
+	socket, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	if err != nil {
+		log.Fatal("Unable to connect to SSH_AUTH_SOCK: ", err)
+	}
+
+	conn := agent.NewClient(socket)
+	if err := conn.Add(addedKey); err != nil {
+		log.Fatal("Unable to load SSH keys into the SSH agent: ", err)
+	}
+}
+
+func Authenticate(client http.Client, endpoint, username, password string) (string, error) {
+	token := TokenAuthResponse{}
+
+	data := url.Values{}
+	data.Set("username", username)
+	data.Set("password", password)
+
+	req, _ := http.NewRequest("POST", endpoint, bytes.NewBufferString(data.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if resp.StatusCode != 200 {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		return "", fmt.Errorf("%v", string(body))
+	}
+
+	json.NewDecoder(resp.Body).Decode(&token)
+
+	resp.Body.Close()
+
+	return token.Token, nil
+}
+
+func SignCertificate(client http.Client, endpoint, token, opt, publicKey string) SingedAuthResponse {
+	keypair := SingedAuthResponse{}
+
+	payload := map[string]string{"public": publicKey}
+
+	jsonPayload, _ := json.Marshal(payload)
+
+	req, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonPayload))
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Token %s", token))
+	req.Header.Add("X-MiruSSH-OTP", opt)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Failed to request signed certificate: %v\n", err)
+	}
+
+	if resp.StatusCode != 200 {
+		body, _ := ioutil.ReadAll(resp.Body)
+		log.Fatal(string(body))
+	}
+
+	json.NewDecoder(resp.Body).Decode(&keypair)
+
+	defer resp.Body.Close()
+
+	return keypair
+}
---
+++ cmd/root.go
@@ -0,0 +1,106 @@
+// Copyright © 2018 Damian Myerscough <Damian.Myerscough@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"bufio"
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
+	"github.com/dmyerscough/mirussh-client/client"
+	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"syscall"
+)
+
+const MIRUSSHURL = "https://mirulabs.xyz"
+
+var (
+	cfgFile  string
+	username string
+	keysize  int
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "mirussh-client",
+	Short: "MiruSSH Certificate Authentication",
+	Long: `Provisions a SSH certificate valid for a certin period of time and loads
+the certificate into the SSH agent.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var endpoint string
+
+		if url := os.Getenv("MIRUSSH"); url != "" {
+			endpoint = url
+		} else {
+			endpoint = MIRUSSHURL
+		}
+
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Print("Enter password: ")
+		bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
+
+		fmt.Print("\nEnter MFA token: ")
+
+		otp, _ := reader.ReadString('\n')
+		otp = strings.TrimSuffix(otp, "\n")
+
+		key, err := rsa.GenerateKey(rand.Reader, keysize)
+		if err != nil {
+			log.Fatal("Unable to generate SSH key pair: %v", err)
+		}
+
+		publicKey, err := ssh.NewPublicKey(&key.PublicKey)
+		if err != nil {
+			log.Fatalf("Unsupported key type: %v", err)
+		}
+
+		token, err := client.Authenticate(http.Client{}, endpoint+"/auth/", username, string(bytePassword))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cert := client.SignCertificate(http.Client{}, fmt.Sprintf(endpoint+"/management/sign/"), token, otp, string(ssh.MarshalAuthorizedKey(publicKey)))
+
+		sshCertificate, _, _, _, _ := ssh.ParseAuthorizedKey([]byte(cert.Certificate))
+
+		sshCert, _ := sshCertificate.(*ssh.Certificate)
+
+		client.ConfigureSSHAgent(key, sshCert, cert.Username, cert.Ttl)
+		log.Println("SSH certificate successfully added to your ssh-agent")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&username, "username", "u", "", "Login username")
+	rootCmd.Flags().IntVarP(&keysize, "keysize", "k", 4096, "SSH key size")
+
+	rootCmd.MarkFlagRequired("username")
+}
